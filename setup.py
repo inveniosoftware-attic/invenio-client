f2ac42c282ae0845d23e919acb6ccc85d92d9f59
Alizee Pace <alizee.pace@gmail.com>
2016-08-24 11:53:19 +0200

    installation: addition of PyPy classifiers
    Signed-off-by: Alizee Pace <alizee.pace@gmail.com>

--- setup.py
+++ setup.py
@@ -75,9 +75,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v2 '
